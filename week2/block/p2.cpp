a3c915448ae6f0b29152c70452336eb9bb8d515b
a22057916w <a22057916w@gmail.com>
2019-11-02 00:17:41 +0800

    block/p2.cpp finished

--- week2/block/p2.cpp
+++ week2/block/p2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <algorith>
+#include <algorithm>
 #include <cstring>
 using namespace std;
 
@@ -12,15 +12,19 @@ int box[maxn], len[maxn], col[maxn];
 
 // pos for recording the length of differnet consecutive color interval
 int pos;
+
+// dp(i, j) stands for the maximum score in the interval [i, j]
+// an additional condition k in dp(i, j, k) stands for moving right k-th index
+// to get an maximum score on dp[i][j][k]
 int dp[maxn][maxn][maxn];
 
-void init() {
+void init(int n) {
   pos = 0;
   len[pos] = 1;
   col[pos] = box[0];
 
   int prev = box[0];
-  for(int i = 0; i < n; i++) {
+  for(int i = 1; i < n; i++) {
     if(box[i] == prev)
       len[pos]++;
     else {
@@ -35,31 +39,47 @@ void init() {
 
 int DP(int l, int r, int k) {
 
-  //
+  // terminal condition
+  if(l > r)
+    return 0;
+
+  // return the score if it's calculated and determined
+  if(dp[l][r][k] != - 1)
+    return dp[l][r][k];
+
+  // find the maximum socre of interval [l, r - 1] plus the score of interval [r, k]
+  // because three might be the same color in the interval [r, k] after merge
   int ret = DP(l, r - 1, 0) + (len[r] + k) * (len[r] + k);
 
-  for(int i = l; i < r; i--)
+  // there might be the same color in the interval [l, r] after merge,
+  // therefore, we need to find the interval with maximum socre that lies in the interval [l, r]
+  for(int i = l; i < r; i++)
     if(col[i] == col[r])
       ret = max(ret, DP(i + 1, r - 1, 0) + DP(l, i, len[r] + k));
+  dp[l][r][k] = ret;
 
+  return ret;
 }
 
 int main() {
-  //freopen("10559.in", "r", stdin);
+  freopen("10559.in", "r", stdin);
 
-  int testcase;
+  int testcase, cas = 1;
   cin >> testcase;
 
   while(testcase--) {
 
     int n;
+    cin >> n;
+
     for(int i = 0; i < n; i++)
       cin >> box[i];
 
     // initiating the params box, len, col, dp, and pos
-    init();
+    init(n);
 
-
+    // perform the DP and print the maximum score
+    cout << "Case " << cas++ << ": " << DP(0, pos, 0) << endl;
   }
 
   return 0;
