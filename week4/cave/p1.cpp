3320affb5886d65ff201cf6a6a61b3fe39bcdf30
a22057916w <a22057916w@gmail.com>
2019-11-03 19:01:40 +0800

    cave/p1.cpp finished

--- week4/cave/p1.cpp
+++ week4/cave/p1.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+const int maxn = 1000005;
+int f[maxn], c[maxn], h[maxn];
+
 int main() {
+  freopen("1442.in", "r", stdin);
+
+  int testcase;
+  cin >> testcase;
+
+  while(testcase--) {
+      int n;
+      cin >> n;
+      for(int i = 0; i < n; i++)
+        cin >> f[i];
+      for(int i = 0; i < n; i++)
+        cin >> c[i];
+
+      int ans = 0, level = c[0];
+      // sweeping the cave twice and recording the storable height of oil
+      // first, left to right
+      for(int i = 0; i < n; i++) {
+        if(f[i] > level)
+          level = f[i];
+        if(c[i] < level)
+          level = c[i];
+        h[i] = level;
+      }
+      // second, right to left
+      level = c[n - 1];
+      for(int i = n - 1; i >= 0; i--) {
+        if(f[i] > level)
+          level = f[i];
+        if(c[i] < level)
+          level = c[i];
+        ans += min(h[i], level) - f[i];
+      }
+
+      // print out the answer
+      cout << ans << endl;
+  }
+
   return 0;
 }
