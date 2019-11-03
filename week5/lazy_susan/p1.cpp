2feba60a3ad7462ee0bb25a26cd9298d268133c1
a22057916w <a22057916w@gmail.com>
2019-11-03 23:07:50 +0800

    lazy_susan/p1.cpp finished

--- week5/lazy_susan/p1.cpp
+++ week5/lazy_susan/p1.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+const int maxn = 505;
+int a[maxn];
+
 int main() {
+  freopen("1620.in", "r", stdin);
+
+  int testcase;
+  cin >> testcase;
+
+  while(testcase--) {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+      cin >> a[i];
+
+    int cnt = 0; // 存逆序數
+    for(int i = 0; i < n; i++)
+      for(int j = i + 1; j < n; j++)
+        if(a[i] > a[j])
+          cnt++;
+
+    if(cnt % 2 && n % 2)
+      cout << "impossible" << endl;
+    else
+      cout << "possible" << endl;
+  }
   return 0;
 }
