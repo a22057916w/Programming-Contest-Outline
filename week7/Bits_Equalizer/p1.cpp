b9245ce351e87d22d2ca1f24aac6a88a574fe06d
a22057916w <a22057916w@gmail.com>
2019-11-05 17:17:39 +0800

    add comment

--- week7/Bits_Equalizer/p1.cpp
+++ week7/Bits_Equalizer/p1.cpp
@@ -14,7 +14,10 @@ int main() {
     string s, t;
     cin >> s >> t;
 
+    // recording the number of '1' and '?' in s as s1 and que
+    // recording the number of '1' in t as t1
     int s1 = 0, t1 = 0, que = 0;
+    // recording the number of pair (1, 0) and (0, 1) as one_zero and zero_one
     int one_zero = 0;
     int zero_one = 0;
     for(int i = 0; i < s.size(); i++) {
@@ -30,8 +33,12 @@ int main() {
         zero_one++;
     }
 
+    // if the number of '1' in s is more than that in t, then there is no solution
     if(s1 > t1)
       printf("Case %d: -1\n", cas++);
+    // performing the exchanges of pairs between all (1, 0), (0, 1),
+    // then turn the question mark into the corelated sign and exchange the pairs again,
+    // therefore, all (1, 0) must count and also the question mark
     else
       printf("Case %d: %d\n", cas++, max(one_zero, zero_one) + que);
   }
