fca13fc34c43ee1a448bc41cfd68bc3a310ac800
a22057916w <a22057916w@gmail.com>
2019-11-06 12:54:50 +0800

    p2 finished

--- week1/Qin_Shi_Huangs_National_Road_System/p2.cpp
+++ week1/Qin_Shi_Huangs_National_Road_System/p2.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
+#include <vector>
 using namespace std;
 
 const int maxn = 1010;
@@ -20,6 +21,8 @@ struct Edge {
     return weight < e.weight;
   }
 } edge[maxn * maxn];
+
+vector<Edge> tree[maxn];
 // recording the city status
 struct City {
   double x, y;
@@ -31,7 +34,9 @@ struct City {
 
 // recording the parents for each node(city)
 int p[maxn];
-double mstEdge[maxn][maxn];
+
+int visited[maxn];
+double maxEdge[maxn][maxn];
 
 void init(int n) {
   for(int i = 0; i < n; i++)
@@ -47,9 +52,11 @@ void init(int n) {
   // ordering the sequence from smaller(edge's weight) to bigger for the use of "kruscal" algorithm
   sort(edge, edge + e);
   // set each node's parent as itself
-  for(int i = 0; i < n; i++)
+  for(int i = 0; i < n; i++) {
     p[i] = i;
-  memset(mstEdge, 0, sizeof(mstEdge));
+    tree[i].clear();
+    visited[i] = false;
+  }
 }
 
 // find node x's parents
@@ -64,8 +71,7 @@ void joint(int a, int b) {
 double kruscal(int n) {
   double sum = 0;
 
-  int i, j;
-  for(i = 0, j = 0; i < n - 1 && j < e; i++, j++) {
+  for(int i = 0, j = 0; i < n - 1 && j < e; i++, j++) {
     // check if the node a and b on a edge have the same parents
     // if so, they are in the same tree, then continue to the next edge
     while(find(edge[j].a) == find(edge[j].b)) {
@@ -75,9 +81,31 @@ double kruscal(int n) {
     joint(edge[j].a, edge[j].b);
     // once the edge has been connected, we can count the weight of edge
     sum += edge[j].weight;
-    mstEdge[edge[j].a][edge[j].b] = edge[j].weight;
+    tree[edge[j].a].push_back(Edge(edge[j].a, edge[j].b, edge[j].weight));
+    tree[edge[j].b].push_back(Edge(edge[j].b, edge[j].a, edge[j].weight));
   }
   return sum;
+}
+
+void dfs(int u, int n) {
+  // traverse the MST by DFS like,
+  // node 0 to 1 -> node 1 to 2 and node 1 back to 0
+  // then find the maximun edge of node 1 between node 0 and 2
+  visited[u] = true;
+  for(int i = 0; i < tree[u].size(); i++) {
+    int v = tree[u][i].b;
+    if(visited[v])
+      continue;
+
+    double cost = tree[u][i].weight;
+    maxEdge[u][v] = maxEdge[v][u] = cost;
+
+    for(int j = 0; j < n; j++) {
+      if(visited[j])
+        maxEdge[j][v]= maxEdge[v][j] = max(maxEdge[j][u], cost);
+    }
+    dfs(v, n);
+  }
 }
 
 int main() {
@@ -93,10 +121,14 @@ int main() {
       init(n);
 
       double mstCost = kruscal(n);
+      // perform dfs to find the maximun wieght for a node to the adjcent nodes on MST
+      dfs(0, n);
+
+      // find out the maximun ration and print out the answer
       double ans = 0;
       for(int i = 0; i < n; i++)
         for(int j = i + 1; j < n; j++)
-          ans = max(ans, (city[i].p + city[j].p) / (mstCost - mstEdge[i][j]));
+          ans = max(ans, (city[i].p + city[j].p) / (mstCost - maxEdge[i][j]));
       printf("%.2lf\n", ans);
   }
 
